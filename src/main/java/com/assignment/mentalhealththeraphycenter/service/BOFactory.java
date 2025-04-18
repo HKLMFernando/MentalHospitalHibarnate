7e9b1cca94fc1b4174e5521839c226505c4f6b5b
HKLMFernando <matheeshafernando61@gmail.com>
2025-04-18 09:12:07 +0530

    FactoryConfiguration updsate

---
+++ src/main/java/com/assignment/mentalhealththeraphycenter/service/BOFactory.java
@@ -0,0 +1,4 @@
+package com.assignment.mentalhealththeraphycenter.service;
+
+public class BOFactory {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,26 @@
 module com.assignment.mentalhealththeraphycenter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
+    requires java.mail;
+    requires mysql.connector.j;
     requires jakarta.persistence;
-    requires static lombok;
+    requires java.naming;
+    requires org.hibernate.orm.core;
+    requires  lombok;
+    opens com.assignment.mentalhealththeraphycenter.config to jakarta.persistence;
+    opens com.assignment.mentalhealththeraphycenter.entity to org.hibernate.orm.core;
+
+    exports com.assignment.mentalhealththeraphycenter.entity;
 
 
     opens com.assignment.mentalhealththeraphycenter to javafx.fxml;
     exports com.assignment.mentalhealththeraphycenter;
+    exports com.assignment.mentalhealththeraphycenter.controller;
+    opens com.assignment.mentalhealththeraphycenter.controller to javafx.fxml;
+    opens com.assignment.mentalhealththeraphycenter.dto.TM to javafx.base;
+
+    exports com.assignment.mentalhealththeraphycenter.dto to org.hibernate.orm.core;
+
 }
